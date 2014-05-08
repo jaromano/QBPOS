3aaac21620762eb86d02871a08e0c7eeaea12664
Peter Fletcher <pfletcher@DT-100159.(none)>
2014-05-08 09:34:33 -0700

    inital uplaod

---
+++ mapel/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnFindServer = New System.Windows.Forms.Button()
+        Me.txtServer = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnFindServer
+        '
+        Me.btnFindServer.Location = New System.Drawing.Point(51, 49)
+        Me.btnFindServer.Name = "btnFindServer"
+        Me.btnFindServer.Size = New System.Drawing.Size(161, 31)
+        Me.btnFindServer.TabIndex = 0
+        Me.btnFindServer.Text = "btnFindServer"
+        Me.btnFindServer.UseVisualStyleBackColor = True
+        '
+        'txtServer
+        '
+        Me.txtServer.Location = New System.Drawing.Point(45, 13)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(200, 20)
+        Me.txtServer.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 273)
+        Me.Controls.Add(Me.txtServer)
+        Me.Controls.Add(Me.btnFindServer)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnFindServer As System.Windows.Forms.Button
+    Friend WithEvents txtServer As System.Windows.Forms.TextBox
+
+End Class
---
+++ mapel/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+    Dim connString As String
+    Dim filePath As String
+
+    'Dim oFSO As New Scripting.FileSystemObject
+    'Dim oTS As Scripting.TextStream
+
+    Dim fcon As Boolean
+    Dim serverName As String
+    Dim coName As String
+    Dim versionName As String
+    Dim currentVersion As Integer
+    Dim qbposXMLRP As QBPOSXMLRPLib.RequestProcessor
+
+    Private Sub Form1_Load()
+        ' Step-1: When the form is loaded, reading from
+        ' a text file (or you could use registry) to get the last stored
+        ' connection string
+        connString = ""
+        filePath = "cstr.txt"
+        'oFSO = New Scripting.FileSystemObject
+        'connString = FileRead
+        'LabelConnString.Caption = connString
+        'qbposXMLRP = New RequestProcessor
+    End Sub
+
+
+    Private Sub btnFindServer_Click(sender As Object, e As EventArgs) Handles btnFindServer.Click
+        qbposXMLRP = getRequestProcessor()
+        'cmdStart.Caption = "Searching. Please wait..."
+        getServers(qbposXMLRP)
+        'cmdStart.Caption = "Done"
+        'cmdServer.Enabled = True
+    End Sub
+
+    Public Function getRequestProcessor() As QBPOSXMLRPLib.RequestProcessor
+        getRequestProcessor = qbposXMLRP
+    End Function
+
+
+    Private Sub getServers(qbposXMLRP As QBPOSXMLRPLib.RequestProcessor)
+        Dim posserversXML As String
+
+        'Method signature for POSServers():
+        'HRESULT POSServers([in] VARIANT_BOOL IsPractice, [out,retval] BSTR* ServersXML);
+        '
+        'Returns data about the available QBPOS instances, companies, and versions in the local network.
+        '
+        'Where,
+        'IsPractice
+        'Specify True if the company is the QBPOS practice company. This can decrease the lookup time by restricting the search to the test company. Useful for development. For real deployments, however, this value would be set to False to make sure all companies were listed.
+        '
+        'ServersXML
+        'Pointer to the returned string containing an XML document containing the list of available servers.
+        '
+        'An example of what is returned as ServersXML from POSServers()
+        '
+        '<POSServers>
+        '<POSServer>
+        '    <ServerName>mtvd040202111</ServerName>
+        '    <CompanyName>al&apos;s sports hut</CompanyName>
+        '    <Version>4</Version>
+        '</POSServer>
+        '<POSServer>
+        '    <ServerName>mtvl040202118</ServerName>
+        '    <CompanyName>al&apos;s sports hut</CompanyName>
+        '    <Version>4</Version>
+        '</POSServer>
+        '...
+        '</POSServers>
+
+        posserversXML = qbposXMLRP.POSServers(False)
+        Dim xmlDoc As MSXML2.DOMDocument40
+        Dim objNodeList As MSXML2.IXMLDOMNodeList
+        Dim objChild As MSXML2.IXMLDOMNode
+        Dim childNode As MSXML2.IXMLDOMNode
+
+        Dim i As Integer
+        Dim ret As Boolean
+        Dim errorMsg As String
+
+        errorMsg = ""
+        xmlDoc = New MSXML2.DOMDocument40
+
+        ret = xmlDoc.loadXML(posserversXML)
+        If Not ret Then
+            errorMsg = "loadXML failed, reason: " & xmlDoc.parseError.reason
+            GoTo ErrHandler
+        End If
+
+        Dim server As String
+        objNodeList = xmlDoc.getElementsByTagName("POSServer")
+        For i = 0 To (objNodeList.length - 1)
+            For Each objChild In objNodeList.Item(i).childNodes
+                If objChild.nodeName = "ServerName" Then
+                    server = objChild.Text
+                End If
+                If objChild.nodeName = "CompanyName" Then
+                    server = server + " - " + objChild.Text
+                End If
+                If objChild.nodeName = "Version" Then
+                    server = server + " - " + objChild.Text
+                End If
+                'ComboServer.List(i) = server
+            Next
+        Next
+        Exit Sub
+
+ErrHandler:
+        MsgBox(Err.Description, vbExclamation, "Error")
+        Exit Sub
+    End Sub
+
+
+End Class
