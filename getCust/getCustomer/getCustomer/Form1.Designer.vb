55a5e126ddc54c46a45e3d904cb887338791ac8c
Peter Fletcher <pfletcher@DT-100159.(none)>
2014-05-13 15:55:50 -0700

    Add getCust directory

---
+++ getCust/getCustomer/getCustomer/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCustomers = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCompany = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCustomers
+        '
+        Me.btnCustomers.Location = New System.Drawing.Point(58, 16)
+        Me.btnCustomers.Name = "btnCustomers"
+        Me.btnCustomers.Size = New System.Drawing.Size(113, 39)
+        Me.btnCustomers.TabIndex = 0
+        Me.btnCustomers.Text = "Get Customers"
+        Me.btnCustomers.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Store Info"
+        '
+        'btnCompany
+        '
+        Me.btnCompany.Location = New System.Drawing.Point(211, 17)
+        Me.btnCompany.Name = "btnCompany"
+        Me.btnCompany.Size = New System.Drawing.Size(107, 37)
+        Me.btnCompany.TabIndex = 2
+        Me.btnCompany.Text = "Get Company"
+        Me.btnCompany.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(254, 226)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(86, 34)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(559, 273)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCompany)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCustomers)
+        Me.Name = "Form1"
+        Me.Text = "Get From QuickBooks POS"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCustomers As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnCompany As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ getCust/getCustomer/getCustomer/Form1.vb
@@ -0,0 +1,241 @@
+﻿
+Imports QBPOSFC3Lib
+'Imports QBFC12Lib
+
+Public Class Form1
+    Dim sessionBegun As Boolean = False
+    Dim connectionOpen As Boolean = False
+    Dim sessionManager As QBPOSSessionManager = Nothing
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            'Create the session Manager object
+            sessionManager = New QBPOSSessionManager
+
+            'Connect ot QB_POS and begin session
+            sessionManager.OpenConnection("mapel", "test")
+            sessionManager.BeginSession("")
+            connectionOpen = True
+            sessionBegun = True
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error")
+            If (sessionBegun) Then
+                sessionManager.EndSession()
+            End If
+            If (connectionOpen) Then
+                sessionManager.CloseConnection()
+            End If
+        End Try
+    End Sub
+
+    Private Sub btnCustomers_Click(sender As Object, e As EventArgs) Handles btnCustomers.Click
+        Try
+            'Create the message set request object to hold the request
+            Dim requestMsgSet As IMsgSetRequest
+            requestMsgSet = sessionManager.CreateMsgSetRequest(3, 0)
+            requestMsgSet.Attributes.OnError = QBPOSFC3Lib.ENRqOnError.roeContinue
+            BuildCustomerQueryRq(requestMsgSet)
+
+            'Send the request and get the response from QuickBooks
+            Dim responseMsgSet As IMsgSetResponse
+            responseMsgSet = sessionManager.DoRequests(requestMsgSet)
+
+            WalkCustomerQueryRs(responseMsgSet)
+
+        Catch er As Exception
+            MessageBox.Show(er.Message, "Error")
+            If (sessionBegun) Then
+                sessionManager.EndSession()
+            End If
+            If (connectionOpen) Then
+                sessionManager.CloseConnection()
+            End If
+        End Try
+    End Sub
+    Public Sub BuildCustomerQueryRq(requestMsgSet As IMsgSetRequest)
+        Dim CustomerQueryRq As ICustomerQuery
+        Dim dtFrom As DateTime = New DateTime(2014, 4, 26)
+        Dim dtTo As DateTime = DateTime.Now
+
+        CustomerQueryRq = requestMsgSet.AppendCustomerQueryRq()
+        'CustomerQueryRq.MaxReturned.SetValue(6)
+
+        CustomerQueryRq.OwnerIDList.Add(0)
+
+
+        CustomerQueryRq.ORTimeCreatedFilters.TimeCreatedRangeFilter.FromTimeCreated.SetValue(dtFrom, False)
+        CustomerQueryRq.ORTimeCreatedFilters.TimeCreatedRangeFilter.ToTimeCreated.SetValue(dtTo, False)
+
+        'CustomerQueryRq.ORFirstNameFilters.FirstNameRangeFilter.FromFirstName.SetValue("Peter")
+        'CustomerQueryRq.ORFirstNameFilters.FirstNameRangeFilter.ToFirstName.SetValue("Peter")
+
+    End Sub
+    Public Sub WalkCustomerQueryRs(responseMsgSet As IMsgSetResponse)
+        If (responseMsgSet Is Nothing) Then
+            Exit Sub
+        End If
+        Dim strReturn As String = responseMsgSet.ToXMLString()
+        Dim responseList As IResponseList
+        'MsgBox(strReturn)
+        responseList = responseMsgSet.ResponseList
+        If (responseList Is Nothing) Then
+            Exit Sub
+        End If
+
+        Dim j As Integer = 0
+        'if we sent only one request, there is only one response, we'll walk the list for this sample
+        For j = 0 To responseList.Count - 1
+            Dim response As IResponse
+            response = responseList.GetAt(j)
+            'check the status code of the response, 0=ok, >0 is warning
+            If (response.StatusCode >= 0) Then
+                'the request-specific response is in the details, make sure we have some
+                If (Not response.Detail Is Nothing) Then
+                    'make sure the response is the type we're expecting
+                    Dim responseType As ENResponseType
+                    responseType = CType(response.Type.GetValue(), ENResponseType)
+                    If (responseType = ENResponseType.rtCustomerQueryRs) Then
+                        '//upcast to more specific type here, this is safe because we checked with response.Type check above
+
+                        Dim CustomerRet As ICustomerRetList = CType(response.Detail, ICustomerRetList)
+                        WalkCustomerRet(CustomerRet)
+                    End If
+                End If
+            End If
+        Next j
+    End Sub
+    Public Sub WalkCustomerRet(CustomerRet As ICustomerRetList)
+        If (CustomerRet Is Nothing) Then
+            Exit Sub
+        End If
+
+        Dim intItems As Integer = CustomerRet.Count
+        Dim StrFullNames As String = ""
+        For i = 0 To CustomerRet.Count - 1
+            'Dim ListID481 As String = CustomerRet.GetAt(0).ListID.GetValue()
+            'ListID481 = CustomerRet.ListID.GetValue
+            Dim strFirstName As String
+            Dim strLastName As String
+            'Go through all the elements of ICustomerRet
+            If (Not CustomerRet.GetAt(i).FirstName Is Nothing) Then
+                strFirstName = CustomerRet.GetAt(i).FirstName.GetValue()
+            Else
+                strFirstName = " "
+            End If
+            If (Not CustomerRet.GetAt(i).LastName Is Nothing) Then
+                strLastName = CustomerRet.GetAt(i).LastName.GetValue()
+            Else
+                strLastName = " "
+            End If
+
+            StrFullNames = StrFullNames & Environment.NewLine & strFirstName & " " & strLastName
+        Next i
+        Label1.Text = StrFullNames
+    End Sub
+
+
+    Private Sub btnCompany_Click(sender As Object, e As EventArgs) Handles btnCompany.Click
+        Try
+            'Create the message set request object to hold the request
+            Dim requestMsgSet As IMsgSetRequest
+            requestMsgSet = sessionManager.CreateMsgSetRequest(3, 0)
+            requestMsgSet.Attributes.OnError = QBPOSFC3Lib.ENRqOnError.roeContinue
+            BuildCompanyQueryRq(requestMsgSet)
+
+            'Send the request and get the response from QuickBooks
+            Dim responseMsgSet As IMsgSetResponse
+            responseMsgSet = sessionManager.DoRequests(requestMsgSet)
+
+            WalkCompanyQueryRs(responseMsgSet)
+
+        Catch er As Exception
+            MessageBox.Show(er.Message, "Error")
+            If (sessionBegun) Then
+                sessionManager.EndSession()
+            End If
+            If (connectionOpen) Then
+                sessionManager.CloseConnection()
+            End If
+        End Try
+    End Sub
+    Public Sub BuildCompanyQueryRq(requestMsgSet As IMsgSetRequest)
+        Dim CompanyQueryRq As ICompanyQuery
+        CompanyQueryRq = requestMsgSet.AppendCompanyQueryRq()
+
+        CompanyQueryRq.OwnerIDList.Add(0)
+    End Sub
+    Private Sub WalkCompanyQueryRs(responseMsgSet As IMsgSetResponse)
+        If (responseMsgSet Is Nothing) Then
+            Exit Sub
+        End If
+
+        Dim responseList As IResponseList
+        responseList = responseMsgSet.ResponseList
+        If (responseList Is Nothing) Then
+            Exit Sub
+        End If
+
+        'if we sent only one request, there is only one response, we'll walk the list for this sample
+        For j = 0 To responseList.Count - 1
+            Dim response As IResponse
+            response = responseList.GetAt(j)
+            'check the status code of the response, 0=ok, >0 is warning
+            If (response.StatusCode >= 0) Then
+                'the request-specific response is in the details, make sure we have some
+                If (Not response.Detail Is Nothing) Then
+                    'make sure the response is the type we're expecting
+                    Dim responseType As ENResponseType
+                    responseType = CType(response.Type.GetValue(), ENResponseType)
+                    If (responseType = ENResponseType.rtCompanyQueryRs) Then
+                        '//upcast to more specific type here, this is safe because we checked with response.Type check above
+                        Dim CompanyRet As ICompanyRet
+                        CompanyRet = CType(response.Detail, ICompanyRet)
+                        WalkCompanyRet(CompanyRet)
+                    End If
+                End If
+            End If
+        Next j
+    End Sub
+    Public Sub WalkCompanyRet(CompanyRet As ICompanyRet)
+        If (CompanyRet Is Nothing) Then
+            Exit Sub
+        End If
+
+        'Go through all the elements of ICompanyRet
+        'Get value of IsSampleCompany
+        Dim IsSampleCompany82 As Boolean
+        IsSampleCompany82 = CompanyRet.IsSampleCompany.GetValue()
+        Dim CompanyName83 As String = CompanyRet.CompanyName.GetValue()
+        Dim Street84 As String = CompanyRet.Address.Street.GetValue()
+        Dim CityStateZIP85 As String = CompanyRet.Address.CityStateZIP.GetValue()
+        Dim Misc186 As String = CompanyRet.Address.Misc1.GetValue()
+        Dim StoreNumber90 As Integer = CompanyRet.StoreNumber.GetValue()
+
+        'Get value of QuickBooksCompanyFile
+        Dim QuickBooksCompanyFile89 As String = ""
+        If (Not CompanyRet.QuickBooksCompanyFile Is Nothing) Then
+            QuickBooksCompanyFile89 = CompanyRet.QuickBooksCompanyFile.GetValue()
+        End If
+
+
+        Dim fullAdd = CompanyName83 & Environment.NewLine & Street84 & Environment.NewLine & CityStateZIP85
+        fullAdd = fullAdd & Environment.NewLine & Misc186 & Environment.NewLine & QuickBooksCompanyFile89
+        fullAdd = fullAdd & Environment.NewLine & StoreNumber90
+
+        Label1.Text = fullAdd
+
+    End Sub
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'End the session and close the connection to QuickBooks
+        sessionManager.EndSession()
+        sessionBegun = False
+        sessionManager.CloseConnection()
+        connectionOpen = False
+
+        Me.Close()
+    End Sub
+
+End Class
